cb197bfee3f5dfc6e0d2e0c8bdd0961e22fae8a9
Lora Edens <lodushanova@gmail.com>
2025-10-09 18:37:06 -0700

    Added has solution to the permutation problem.

--- 1.2_permutations/main.c
+++ 1.2_permutations/main.c
@@ -33,12 +33,42 @@ bool permutation(char* word1, char* word2) {
     return true;
 }
 
+bool permHashChar(char* word1, char* word2) {
+    if (strlen(word1) != strlen(word2)) {
+        return false;
+    }else {
+        // note: does not have O(n) to fill with 0's because on stack and optimized local variable
+        char arr[26] = {0};
+        while (*word1 != '\0') {
+            int index = *word1 - 'a';
+            arr[index]++;
+            word1++;
+        }
+        while(*word2 != '\0') {
+            int index2 = *word2 - 'a';
+            arr[index2]--;
+            if (arr[index2] < 0) {
+                return false;
+            }
+            word2++;
+        }
+        return true;
+    }
+}
 
 int main(void) {
-    char string1[] = "ottoman";
+    char string1[] = "otto";
     char string2[] = "toto";
+
+    // first solution, sorting
     bool result = permutation(string1, string2);
     printf("%s\n", result ? "true" : "false");
+
+    // second solution, hashmap of characters
+    bool result2 = permHashChar(string1, string2);
+    printf("%s\n", result2 ? "true" : "false");
+
+
     //printf("%lu", sizeof(int));
     return 0;
 }
